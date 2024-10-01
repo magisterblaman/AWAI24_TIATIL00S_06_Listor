ba92b4e3d7051cd1bbbd7b922749c79bae207fd2
magisterblaman <hannes.blaman@lbs.se>
2024-10-01 09:59:53 +0200

    insert, del, tilldelning

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-gross_foods = [
-    "lakrits",
-    "skolmaten",
-    "blodpudding",
-    "pyttipanna"
-]
+prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 47]
 
-print(gross_foods)
-gross_foods.append("tacos")
-gross_foods.append("köttfärssås")
-gross_foods.append("pasta")
-gross_foods.append("chili con carne")
-gross_foods.append("FISK")
-print(gross_foods[6])
+prime_numbers.append(53)
+
+prime_numbers.insert(6, 15)
+
+print(prime_numbers)
+
+del prime_numbers[6]
+
+prime_numbers[0] = 1
+
+print(prime_numbers)

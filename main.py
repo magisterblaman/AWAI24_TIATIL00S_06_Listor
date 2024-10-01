a98978ff7d007c9b84948ff53712ad48a29312d9
magisterblaman <hannes.blaman@lbs.se>
2024-10-01 09:28:15 +0200

    Lista

---
+++ main.py
@@ -0,0 +1,9 @@
+names = ["Hannes", "Hannes2", "Hanna", "Björne", "Markus"]
+
+for i in range(len(names)):
+    print(names[i])
+
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])

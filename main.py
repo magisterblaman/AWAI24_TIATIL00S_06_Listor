36869f0f6bb9f49f61fba3e7d18057f5a2fa4faa
magisterblaman <hannes.blaman@lbs.se>
2024-10-01 10:09:37 +0200

    in

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 47]
 
-prime_numbers.append(53)
+user_prime_str = input("Vilket tal vill du kolla? ")
+user_prime = int(user_prime_str)
 
-prime_numbers.insert(6, 15)
-
-print(prime_numbers)
-
-del prime_numbers[6]
-
-prime_numbers[0] = 1
-
-print(prime_numbers)
+if user_prime in prime_numbers:
+    print(str(user_prime) + " är ett primtal")
+else:
+    print(str(user_prime) + " är inte ett primtal")

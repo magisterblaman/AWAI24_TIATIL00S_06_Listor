779e90a627a35df13f690015ddeaf7e9e8f85c00
magisterblaman <hannes.blaman@lbs.se>
2024-10-08 09:33:57 +0200

    Övningar 4 - Checkout lösning

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 47]
+unique_numbers = []
 
-user_prime_str = input("Vilket tal vill du kolla? ")
-user_prime = int(user_prime_str)
+run = True
 
-if user_prime in prime_numbers:
-    print(str(user_prime) + " är ett primtal")
-else:
-    print(str(user_prime) + " är inte ett primtal")
+while run:
+    answer = input("Vänligen mata in ett heltal hejhej: ")
+    converted = int(answer)
+    if converted in unique_numbers:
+        while unique_numbers[0] != converted:
+            del unique_numbers[0]
+        for i in range(len(unique_numbers)):
+            print(unique_numbers[i])
+    else:
+        unique_numbers.append(converted)
+
+    answer2 = input("Vill du köra igen? ")
+    if answer2 == "nej":
+        run = False
